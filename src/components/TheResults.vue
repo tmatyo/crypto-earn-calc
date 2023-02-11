bae5153b59f7a070bbc48756b6515ec06c293ecf
tmatyo <tomi.matyo@gmail.com>
2023-02-11 21:29:53 +0100

    results page added

---
+++ src/components/TheResults.vue
@@ -0,0 +1,29 @@
+<script>
+import { useTransactionStore } from '../stores/TransactionStore';
+
+export default {
+    setup() {
+        const tas = useTransactionStore();    
+
+        return { tas }
+    }    
+}
+
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p v-if="!tas.isEmpty">File is uploaded and we CAN work with it :)</p>
+    <p v-if="tas.isEmpty">File is uploaded we CANNOT work with it :(</p>
+    <p>Number of transactions {{ tas.count }}</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+  }
+}
+</style>
