75fc8ca47d0cfd382c2d1ee33d621d9d1b788b1c
tmatyo <tomi.matyo@gmail.com>
2023-02-11 21:12:56 +0100

    transactionStore updated with actions, getters

--- src/stores/TransactionStore.js
+++ src/stores/TransactionStore.js
@@ -2,6 +2,19 @@ import { defineStore } from "pinia";
 
 export const useTransactionStore = defineStore ('transactionStore', {
     state: () => ({
-        transakcions: []
-    })
+        transactions: []
+    }),
+    getters: {
+        count() {
+            return this.transactions.length;
+        },
+        isEmtpy() {
+            return this.transactions.length === 0;
+        }
+    },
+    actions: {
+        addTransactions(ta) {
+            this.transactions.push(ta);
+        }
+    }
 })
