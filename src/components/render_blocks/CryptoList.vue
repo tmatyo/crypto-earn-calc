6bec7a0d22fe8c1d2d5f4fe6174ede32203d1fcd
Vladimir Vladovic <vladimir.vladovic@entaingroup.com>
2023-03-24 13:57:01 +0100

    Update props syntax in CryptoList.vue

--- src/components/render_blocks/CryptoList.vue
+++ src/components/render_blocks/CryptoList.vue
@@ -1,23 +1,26 @@
-<script>
-export default {
-    props: ['data', 'meta'],
-    super(props) {
-        console.log('These are the props: ', props);
-        return { props }
+<script setup>
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true,
+    },
+    meta: {
+        type: Object,
+        required: true,
     }
-}
+})
 </script>
 
 <template>
     <div class="stat-data-block">
-        <!-- <h2>Crypto portfolio 
-            <span class="cec-important">{{ props.meta.aprox_net_worth.toFixed(2) + " " + props.data[0].native_currency }}</span>
+        <h2>Crypto portfolio 
+            <span class="cec-important">{{ props.meta.aprox_net_worth?.toFixed(2) + " " + props.data[0].native_currency }}</span>
         </h2>
         <div class="stat-container">
             <div v-for="d in props.data" class="stat-tile">
-                <p class="data-value">{{ d.amount.toFixed(2) + " " + d.currency}} </p>
-                <small class="data-desc">{{ d.current_worth.toFixed(2) + " " + d.native_currency }}</small>
+                <p class="data-value">{{ d.amount?.toFixed(2) + " " + d.currency}} </p>
+                <small class="data-desc">{{ d.current_worth?.toFixed(2) + " " + d.native_currency }}</small>
             </div>
-        </div> -->
+        </div>
     </div>
 </template>
