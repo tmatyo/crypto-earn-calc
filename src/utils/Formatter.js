d7acb8f4c5d683f5bc1fbc82d7637eaf7645b4e3
Tamás Matyó <tomi.matyo@gmail.com>
2024-07-06 23:10:15 +0200

    fix: fixing formatter for 1 > x > 0

--- src/utils/Formatter.js
+++ src/utils/Formatter.js
@@ -7,7 +7,16 @@
 export const lang = window.navigator.language || window.navigator.userLanguage;
 
 export function formatCurrency(c) {
-	return new Intl.NumberFormat(lang).format((c * 1).toFixed(2));
+	c *= 1;
+	let options = {};
+
+	if (c < 0.01) {
+		options = { maximumFractionDigits: 7 };
+	} else {
+		c = c.toFixed(2);
+	}
+
+	return new Intl.NumberFormat(lang, options).format(c);
 }
 
 export function uniqBy(arr, predicate) {
@@ -17,9 +26,7 @@ export function uniqBy(arr, predicate) {
 		...arr
 			.reduce((map, item) => {
 				const key = item === null || item === undefined ? item : cb(item);
-
 				map.has(key) || map.set(key, item);
-
 				return map;
 			}, new Map())
 			.values(),
